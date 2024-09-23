734de4b15885cbe6712f16b5e419aba68430a081
MiliVives <milivives@gmail.com>
2024-09-23 19:52:48 -0300

    commiteo

---
+++ src/MiCompEntretenimiento.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MiCompEntretenimiento() {
+    // return a jsx ul list of a menu with five random items
+    return <div>
+       <p> Esta es una noticia sobre entretenimiento</p>
+       <p> Recientemente ha tocado Eric Clapton en Argentina.</p>
+       <p> Es hora de escuchar Old Love.</p>
+
+        
+
+    </div>;
+}
+
+export default MiCompEntretenimiento
+
